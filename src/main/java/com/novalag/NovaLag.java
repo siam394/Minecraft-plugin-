03aaa89a86e5b5f4dbee88e880f1a8c8e63cca5a
siam394 <nurulislamnubd339@gmail.com>
2026-07-13 22:31:45 +0600

    Refactor NovaLag class to improve initialization

--- src/main/java/com/novalag/NovaLag.java
+++ src/main/java/com/novalag/NovaLag.java
@@ -1,10 +1,17 @@
 package com.novalag;
 
+import com.novalag.manager.ConfigManager;
+import com.novalag.manager.MessageManager;
+import com.novalag.manager.TaskManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NovaLag extends JavaPlugin {
 
     private static NovaLag instance;
+
+    private ConfigManager configManager;
+    private MessageManager messageManager;
+    private TaskManager taskManager;
 
     @Override
     public void onEnable() {
@@ -12,19 +19,41 @@ public final class NovaLag extends JavaPlugin {
 
         saveDefaultConfig();
 
+        configManager = new ConfigManager(this);
+        messageManager = new MessageManager(this);
+        taskManager = new TaskManager(this);
+
+        taskManager.startTasks();
+
         getLogger().info("================================");
-        getLogger().info("NovaLag Enabled");
-        getLogger().info("Running on Purpur 26.2");
-        getLogger().info("Java 25");
+        getLogger().info("NovaLag Enabled Successfully!");
+        getLogger().info("Java: 25");
+        getLogger().info("Purpur: 26.2");
         getLogger().info("================================");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("NovaLag Disabled");
+        if (taskManager != null) {
+            taskManager.stopTasks();
+        }
+
+        getLogger().info("NovaLag Disabled.");
     }
 
     public static NovaLag getInstance() {
         return instance;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
+
+    public TaskManager getTaskManager() {
+        return taskManager;
     }
 }
